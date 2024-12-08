997069705eea2f4c762682978f4b14412d04060b
eduRepoJD <jacob.adam.davis@und.edu>
2024-12-08 05:31:26 -0800

    Added ThemeManager from Jay's branch

---
+++ CSCI363Project/ThemeManager.cs
@@ -0,0 +1,7 @@
+﻿public static class ThemeManager
+{
+    public static Color CurrentBackColor { get; set; } = Color.White;
+    public static Color CurrentLabelColor { get; set; } = Color.Black;
+    public static Color CurrentTextBoxBackColor { get; set; } = Color.White;
+    public static Color CurrentTextBoxForeColor { get; set; } = Color.Black;
+}
